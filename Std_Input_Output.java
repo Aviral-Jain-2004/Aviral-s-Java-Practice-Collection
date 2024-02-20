95d411f37593d23213759a6d1d64e66189d2cd11
Aviral-Jain-2004 <160483430+Aviral-Jain-2004@users.noreply.github.com>
2024-02-20 07:29:05 +0530

    Add files via upload

---
+++ Std_Input_Output.java
@@ -0,0 +1,23 @@
+// Importing 'Scanner' class from 'java.util' package for reading input.
+import java.util.Scanner;
+
+public class Std_Input_Output {
+
+    public static void main(String[] args) {
+        // Creating a Scanner object.
+        Scanner sc = new Scanner(System.in);
+
+        // Reading inputs.
+        int firstNumber = sc.nextInt();
+        int secondNumber = sc.nextInt();
+        int thirdNumber = sc.nextInt();
+
+        // Displaying output on separate lines using println.
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        System.out.println(thirdNumber);
+
+        // Closeing Scanner object (prevent resource leaks)
+        sc.close();
+    }
+}
