cbbb6cb1a900ac452daf4a1b9d8ebd39f7602b88
Aviral-Jain-2004 <160483430+Aviral-Jain-2004@users.noreply.github.com>
2024-02-20 07:17:38 +0530

    Update and rename Hello_World.java to HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// File name and class name should be same in java.
+public class HelloWorld {
+    // This 'main' method is the entry point of the Java program.
+    public static void main(String[] args) {
+        // Printing message(Hello, World.) to the console.
+        System.out.println("Hello, World.");
+    }
+}
